a45c0e62ff5c60ea0c3364d73b3cb2212c690471
jaynnn <873242707@qq.com>
2023-02-20 19:27:53 +0800

    1.线程管理

--- frame/mgr/thread_mgr.cpp
+++ frame/mgr/thread_mgr.cpp
@@ -3,21 +3,38 @@
 using namespace moob;
 
 //向线程池添加任务 
-void AddTask(std::function<void()> task) { 
-    std::unique_lockstd::mutex lock(m_mutex); 
-    m_task.push_back(task); 
+void ThreadMgr::AddTask(std::function<void()> task)
+{ 
+    std::unique_lockstd::mutex lock(mutex_); 
+    task_.push_back(task); 
 }
 
 //启动线程池，开始执行任务
-void Start(int num)
+void ThreadMgr::Start(int num)
 {
     for (int i = 0; i < num; i++)
-        m_threads.push_back(std::thread(&ThreadManager::Run, this));
+        threads_.push_back(std::thread(&ThreadManager::Run, this));
 }
 
 //等待所有线程完成任务
-void Wait()
+void ThreadMgr::Wait()
 {
-    for (auto &t : m_threads)
+    for (auto &t : threads_)
         t.join();
+}
+
+//每个线程都执行的任务 
+void ThreadMgr::Run() 
+{ 
+    while (true) 
+    { 
+        task_t task; 
+        { 
+            std::unique_lockstd::mutex lock(mutex_); 
+            if (task_.empty()) return; 
+            task = task_.back(); 
+            task_.pop_back(); 
+        } 
+        task(); 
+    } 
 }
--- frame/mgr/thread_mgr.hpp
+++ frame/mgr/thread_mgr.hpp
@@ -5,42 +5,14 @@
 //线程管理类
 class ThreadMgr : MgrInterface
 { 
-    public: ThreadMgr() {}; 
-    ~ThreadMgr(){}; 
-    //向线程池添加任务 
-    void AddTask(std::function<void()> task) { 
-        std::unique_lockstd::mutex lock(mutex_); 
-        task_.push_back(task); 
-    }
-
-    //启动线程池，开始执行任务
-    void Start(int num)
-    {
-        for (int i = 0; i < num; i++)
-            threads_.push_back(std::thread(&ThreadMgr::Run, this));
-    }
-
-    //等待所有线程完成任务
-    void Wait()
-    {
-        for (auto &t : threads_)
-            t.join();
-    }
+    public: 
+        ThreadMgr() {}; 
+        ~ThreadMgr(){}; 
     private: 
-    //每个线程都执行的任务 
-    void Run() { 
-        while (true) { 
-            task_t task; 
-            { 
-                std::unique_lockstd::mutex lock(mutex_); 
-                if (task_.empty()) return; 
-                task = task_.back(); 
-                task_.pop_back(); 
-            } 
-            task(); 
-        } 
-    }
-
+        void AddTask(std::function<void()> task);
+        void Start(int num);
+        void Wait();
+        void Run();
     private: 
         using task_t = std::function<void()>; 
         std::vectorstd::thread threads_; 
